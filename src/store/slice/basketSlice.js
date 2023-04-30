24a43f2aa560b74795cde7d9c8d9c22101a757bf
Lobach Oksana <oksanalobach247@gmail.com>
2023-04-30 11:30:51 +0200

    added localStorage

--- src/store/slice/basketSlice.js
+++ src/store/slice/basketSlice.js
@@ -1,39 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
+const writeToLocalStorage= (basket) =>localStorage.setItem('basket',JSON.stringify(basket))
 
 export const basketSice = createSlice({
   name: "basket",
   initialState: {
-    list: [],
+    list: JSON.parse(localStorage.getItem('basket')) || [],
   },
   reducers: {
     addToBasket: (state, action) => {
       const product = state.list.find((item) => item.id === action.payload);
       if (product === undefined) {
         state.list = [...state.list, { id: action.payload, count: 1 }];
+        writeToLocalStorage(state.list);
       } else {
         product.count++;
         state.list = [...state.list];
+        writeToLocalStorage(state.list);
       }
     },
     deleteBasketItem: (state, action) => {
       state.list = state.list.filter((item) => item.id !== action.payload);
-     
+      writeToLocalStorage(state.list);
     },
     incrementBasket: (state, action) => {
       const product = state.list.find((item) => item.id === action.payload);
       product.count++;
       state.list = [...state.list];
+      writeToLocalStorage(state.list);
     },
     decrementBasket: (state, action) => {
       const product = state.list.find((item) => item.id === action.payload);
       product.count--;
       if (product.count === 0) {
         state.list = state.list.filter((item) => item !== product);
+        writeToLocalStorage(state.list);
       }
       state.list = [...state.list];
+      writeToLocalStorage(state.list);
     },
     clearBasket: (state) => {
       state.list = [];
+      writeToLocalStorage(state.list);
     },
   },
 });
